5eddd55d44cbad90d522836fc727d644ce2f151c
vk-ids <vk-ids@DESKTOP-BM59F75>
2016-04-23 18:30:19 +0530

    Add project files.

---
+++ VSCSharpSolution/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSCSharpSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string InputMessage;
+            Console.WriteLine("Please enter your name ?");
+            InputMessage = Console.ReadLine();
+            Console.WriteLine("Are you {0}", InputMessage);
+            Console.ReadLine();
+        }
+    }
+}
